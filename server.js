d36780d3adb2f9a5a6ac51cf03459b0b7ed29679
Priti Kadam <pritikadam0308@gmail.com>
2025-06-25 11:26:31 +0530

    fix: added cors config

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const cors = require('cors');
+
 dotenv.config();
+
 const app = express();
+app.use(cors());
 app.use(express.json());
 const MONGO_URI = process.env.MONGO_URI;
 if (!MONGO_URI) {
